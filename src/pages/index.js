708cf96072b3916a1d9589ebd972b6c4327c5ef4
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-04-29 12:10:56 +0200

    can see the latest and greatest

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,16 @@
 import Head from "next/head";
 import styles from "./Home.module.css";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import FirstForm from "@/components/FirstForm";
 
-
-export default function Home() { 
-
-
+export default function Home() {
   return (
     <>
       <Head>
         <title>Find buyer | EDC</title>
       </Head>
       <div className="wrapper">
-
         <FirstForm className={styles.form} />
-        
-        
       </div>
     </>
   );
