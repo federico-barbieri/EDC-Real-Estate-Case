72d6ab6f1ec0e69a622468f93157e7503bcb0faf
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-04-30 18:16:32 +0200

    thank you page init

--- src/pages/thanks/index.js
+++ src/pages/thanks/index.js
@@ -1,37 +1,36 @@
-import React from 'react';
-import thanks from "./index.module.css"
+import React from "react";
+import thanks from "./index.module.css";
 import { useRouter } from "next/router";
 
-
 function ThankYou() {
-
-  const {query} = useRouter();
-
-
+  const { query } = useRouter();
 
   console.log(query);
 
-
-
   return (
     <>
-    <div className={thanks.thankYou}>
-    <h2>Thank you!</h2>
-    <p>We have received your information:</p>
-    <ul>
-      <li>User name: <strong>{query.name}</strong></li>
-      <li>User phone: <strong>{query.phone}</strong></li>
-      <li>User email: <strong>{query.email}</strong></li>
-      <li>Interested in these buyers: <strong>{query.postContent}</strong></li>
+      <div className={thanks.thankYou}>
+        <h2>Thank you!</h2>
+        <h3>We have received your request details:</h3>
+        <ul>
+          <li>
+            User name: <strong>{query.name}</strong>
+          </li>
+          <li>
+            User phone: <strong>{query.phone}</strong>
+          </li>
+          <li>
+            User email: <strong>{query.email}</strong>
+          </li>
+          <li>
+            Interested in these buyers: <strong>{query.postContent}</strong>
+          </li>
+        </ul>
 
-
-
-    </ul>
-
-    <button className={thanks.btn}>TAKE ME TO DASHBOARD</button>
-    </div>
+        <button className={thanks.btn}>TAKE ME TO DASHBOARD</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default ThankYou
+export default ThankYou;
