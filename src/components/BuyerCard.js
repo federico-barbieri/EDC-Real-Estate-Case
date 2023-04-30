fc4fb07e59e916eed2291ed2920081f0d57fe3e9
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-04-30 17:26:33 +0200

    general look done

--- src/components/BuyerCard.js
+++ src/components/BuyerCard.js
@@ -1,31 +1,41 @@
-import coolCard from "./BuyerCard.module.css"
-import { useState, useEffect, useRef } from "react"
+import coolCard from "./BuyerCard.module.css";
+import { useState, useEffect, useRef } from "react";
 
-
-export default function BuyerCard({youClickedMe , id, title, description, adults, child, minSize, maxPrice, takeOver }) {
-
+export default function BuyerCard({
+  youClickedMe,
+  id,
+  title,
+  description,
+  adults,
+  child,
+  minSize,
+  maxPrice,
+  takeOver,
+}) {
   const myBtnRef = useRef(null);
 
-  
   return (
     <form id={id} className={coolCard.article}>
-        <h2>{title}</h2>
-        <p>Description: {description}</p>
-        <p>Num of Adults: {adults}</p>
-        <p>Num of children: {child}</p>
-        <p>Max price is: {maxPrice}</p>
-        <p>Min size is: {minSize}</p>
-        <p>Take over date is: {takeOver}</p>
-        
-              
-              <button ref={myBtnRef}  className={coolCard.inputImproved} onClick={() => youClickedMe({id, title, myBtnRef})} type="button" name="checkMe">
-                SELECT ME
-                </button>
-      
+      <h2>{title}</h2>
+      <h4>
+        <span className={coolCard.info}>Description: </span>
+        {description}
+      </h4>
+      <p>Num of Adults: {adults}</p>
+      <p>Num of children: {child}</p>
+      <p>Max price is: {maxPrice}</p>
+      <p>Min size is: {minSize}</p>
+      <p>Takeover date: {takeOver}</p>
+
+      <button
+        ref={myBtnRef}
+        className={coolCard.inputImproved}
+        onClick={() => youClickedMe({ id, title, myBtnRef })}
+        type="button"
+        name="checkMe"
+      >
+        SELECT BUYER
+      </button>
     </form>
-  )
+  );
 }
-
-
-
-
